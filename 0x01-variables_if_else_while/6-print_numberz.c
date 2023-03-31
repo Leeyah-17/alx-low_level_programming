7f306fab2e09dbef8712368545c608fcfb815261
Leeyah-17 <aololade853@gmail.com>
2023-03-30 19:16:13 -0700

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,14 +3,15 @@
 /**
  * main - main function
  *
- * Return: always 0 success
+ * Return: always 0
  */
 
 int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 9; i++)
-		printf("% \n", i);
+	for (i = 0; i < 10; i++)
+		putchar(i + '0');
+	putchar('\n');
 	return (0);
 }
